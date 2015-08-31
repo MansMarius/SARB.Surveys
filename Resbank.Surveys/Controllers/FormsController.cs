664af9b0de92ae1a8e1211b5d0683dd1284e90e7
Marius Mans <marius.mans@bcx.co.za>
2015-08-31 07:30:21 +0200

    rtyert

---
+++ Resbank.Surveys/Controllers/FormsController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Resbank.Surveys.Controllers
+{
+    public class FormsController : ApiController
+    {
+        // GET: api/Forms
+        public string Get()
+        {
+            string HTML = File.ReadAllText(@"c:\users\marius\documents\visual studio 2013\Projects\Resbank.Surveys\Resbank.Surveys\Views\Forms\B08v1.txt");
+            return HTML;
+        }
+
+        // GET: api/Forms/5
+        public string Get(string FormName)
+        {
+            string HTML = File.ReadAllText(@"c:\users\marius\documents\visual studio 2013\Projects\Resbank.Surveys\Resbank.Surveys\Views\Forms\B08v1.txt");
+            return HTML;
+        }
+    }
+}
